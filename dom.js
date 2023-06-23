16de31cd8257c272550e2d50cf8e40068592e0a9
varshagm119 <varshagm96@gmail.com>
2023-06-23 16:55:18 +0530

    manipulatedDom

---
+++ dom.js
@@ -0,0 +1,7 @@
+let headerTitle=document.getElementById('header-title');
+headerTitle.style.borderBottom='solid 3px #000';
+
+
+
+var li= document.getElementsByClassName('title');
+li[0].style.fontWeight=900
