98acd77784303769943d0f8279047a4f119a72e2
varshagm119 <varshagm96@gmail.com>
2023-06-23 22:19:10 +0530

    using query Selector(All)

--- dom.js
+++ dom.js
@@ -1,21 +1,37 @@
-let headerTitle=document.getElementById('header-title');
-headerTitle.style.borderBottom='solid 3px #000';
+// let headerTitle=document.getElementById('header-title');
+// headerTitle.style.borderBottom='solid 3px';
 
 
 
-var Title= document.getElementsByClassName('title');
-Title[0].style.fontWeight=900;
+// var Title= document.getElementsByClassName('title');
+// Title[0].style.fontWeight=900;
 
 
-var li= document.getElementsByClassName('list-group-item');
-li[2].style.backgroundColor='green';
-li[0].style.fontWeight=900;
-li[1].style.fontWeight=900;
-li[2].style.fontWeight=900;
-li[3].style.fontWeight=900;
+// var li= document.getElementsByClassName('list-group-item');
+// li[2].style.backgroundColor='green';
+// li[0].style.fontWeight=900;
+// li[1].style.fontWeight=900;
+// li[2].style.fontWeight=900;
+// li[3].style.fontWeight=900;
 
-var ite = document.getElementsByTagName('li');
-console.log(ite);
-for(var i=0;i<ite.length;i++){
-    ite[i].style.backgroundColor='grey'
+// var ite = document.getElementsByTagName('li');
+// console.log(ite);
+// for(var i=0;i<ite.length;i++){
+//     ite[i].style.backgroundColor='grey'
+// }
+
+
+var secEle=document.querySelector('.list-group-item:nth-child(2)');
+
+secEle.style.color='green';
+
+var thirdEle=document.querySelector('.list-group-item:nth-child(3)');
+
+thirdEle.innerText='';
+
+
+var odd=document.querySelectorAll('.list-group-item:nth-child(odd)');
+
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='#ccc'
 }
