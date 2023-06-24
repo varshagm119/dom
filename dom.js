fe63fd350bf9328ee71b59fee368a41a52be5e24
varshagm119 <varshagm96@gmail.com>
2023-06-24 16:44:17 +0530

    traversing through the dom

--- dom.js
+++ dom.js
@@ -21,17 +21,62 @@
 // }
 
 
-var secEle=document.querySelector('.list-group-item:nth-child(2)');
+// var secEle=document.querySelector('.list-group-item:nth-child(2)');
 
-secEle.style.color='green';
+// secEle.style.color='green';
 
-var thirdEle=document.querySelector('.list-group-item:nth-child(3)');
+// var thirdEle=document.querySelector('.list-group-item:nth-child(3)');
 
-thirdEle.innerText='';
+// thirdEle.innerText='';
 
 
-var odd=document.querySelectorAll('.list-group-item:nth-child(odd)');
+// var odd=document.querySelectorAll('.list-group-item:nth-child(odd)');
 
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='#ccc'
-}
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor='#ccc'
+// }
+
+
+
+
+
+// TRAVERSING THE DOM 
+ var item =document.querySelector('#items');
+
+ console.log(item.parentNode);
+ item.parentNode.style.backgroundColor='#f4f4';
+
+ console.log(item.lastElementChild);
+ item.lastElementChild.innerText='hi 4';
+
+console.log(item.lastChild);
+
+console.log(item.firstElementChild);
+item.firstElementChild.style.color='#ccc';
+
+console.log(item.nextSibling);
+
+console.log(item.nextElementSibling);
+
+console.log(item.previousElementSibling);
+item.previousElementSibling.style.borderBottom='solid 3px'
+
+
+//creating and inserting an element
+
+var newDiv=document.createElement('div');
+newDiv.className='hello';
+newDiv.id='hello1';
+newDiv.setAttribute('title','newly created div');
+ 
+//creating a text node
+var newDivText=document.createTextNode('Hello World');
+
+newDiv.appendChild(newDivText);
+
+//adding this to the header part
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+
+container.insertBefore(newDiv,h1);
+newDiv.style.fontSize='30px';
